e3822264c553a416fa6123f0e9da3489bdf7667a
Anusha Byalyam Ramachandra <anushabyalyam@gmail.com>
2024-08-07 16:43:28 +0530

    Create StudentController.java

---
+++ student-management-backend/src/main/java/com/system/student_management_backend/controller/StudentController.java
@@ -0,0 +1,42 @@
+package com.system.student_management_backend.controller;
+
+import com.system.student_management_backend.model.Student;
+import com.system.student_management_backend.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+@CrossOrigin(origins = "http://localhost:3000")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentService.saveStudent(student);
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable Long id, @RequestBody Student studentDetails) {
+        return studentService.updateStudent(id, studentDetails);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        studentService.deleteStudent(id);
+    }
+}
